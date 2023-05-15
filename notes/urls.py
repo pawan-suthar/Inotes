1b69d449c5950eb8c69453998b46fc77fee9444f
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-05-15 17:49:10 +0530

    user profile page and data added

--- notes/urls.py
+++ notes/urls.py
@@ -4,14 +4,17 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # link for users
     path('', home, name='home'),
     path('about', about, name='about'),
     path('contact', contact, name='contact'),
     path('login', userlogin, name='userlogin'),
-    path('logout', Logout, name='logout'),
     path('signin', signin, name='signin'),
+    path('userprofile', userprofile, name='userprofile'),
+
+    # links for admin
+    path('logout', Logout, name='logout'),
     path('login_admin', login_admin, name='login_admin'),
     path('home_admin', home_admin, name='home_admin'),
-    path('userprofile', userprofile, name='homeuserprofileadmin'),
 
 ]
